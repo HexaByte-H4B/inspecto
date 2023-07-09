739e9800d4b6a0f453812b932098f8f5f010498c
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-07-09 23:14:40 +0530

    feat: added functions

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -5,7 +5,7 @@ import { useContractWrite } from 'wagmi';
 const CONTRACT_ADDRESS = "0x5a8391233E8821621986614ce1C2bcaA1dd5BF3C";
 
 function Main() {
-  /* 
+  /* Apply as auditor
   const { data, isLoading, isSuccess, write } = useContractWrite({
     address: CONTRACT_ADDRESS,
     abi: ensRegistry,
@@ -21,6 +21,28 @@ function Main() {
   return (
     <>
       <button onClick={handleApplyAuditor}>Apply</button>
+      {isLoading && <div>Check Wallet</div>}
+      {isSuccess && <div>Transaction: {JSON.stringify(data)}</div>}
+    </>
+  );
+  */
+
+  /* Assign a auditor
+  const { data, isLoading, isSuccess, write } = useContractWrite({
+    address: CONTRACT_ADDRESS,
+    abi: ensRegistry,
+    functionName: 'assignAuditor',
+  });
+
+  const handleAssignAuditor = async () => {
+    await write({
+      args: ['0', '0']
+    });
+  };
+
+  return (
+    <>
+      <button onClick={handleAssignAuditor}>Assign</button>
       {isLoading && <div>Check Wallet</div>}
       {isSuccess && <div>Transaction: {JSON.stringify(data)}</div>}
     </>
